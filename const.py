bd8773a2ac820fb1f787d3d655e5da7067dbc213
M1R4G376 <166696800+M1R4G376@users.noreply.github.com>
2025-06-20 14:59:21 +0200

    Create const.py

---
+++ const.py
@@ -0,0 +1,9 @@
+DOMAIN = "new_bestway_spa"
+
+CONF_APPID = "appid"
+CONF_APPSECRET = "appsecret"
+CONF_DEVICE_ID = "device_id"
+CONF_PRODUCT_ID = "product_id"
+CONF_REGISTRATION_ID = "registration_id"
+CONF_VISITOR_ID = "visitor_id"
+CONF_CLIENT_ID = "client_id"
