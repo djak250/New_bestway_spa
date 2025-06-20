8fda4b70bb47c185d8730f861d121ac66e05bf22
M1R4G376 <166696800+M1R4G376@users.noreply.github.com>
2025-06-20 14:58:41 +0200

    Create config_flow.py

---
+++ config_flow.py
@@ -0,0 +1,24 @@
+import voluptuous as vol
+from homeassistant import config_entries
+from .const import DOMAIN
+
+class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    VERSION = 1
+
+    async def async_step_user(self, user_input=None):
+        errors = {}
+
+        if user_input is not None:
+            return self.async_create_entry(title=user_input["device_name"], data=user_input)
+
+        schema = vol.Schema({
+            vol.Required("device_name"): str,
+            vol.Required("visitor_id"): str,
+            vol.Required("registration_id"): str,
+            vol.Optional("device_id"): str,
+            vol.Optional("product_id"): str,
+            vol.Optional("push_type", default="fcm"): vol.In(["fcm", "apns"]),
+            vol.Optional("client_id"): str
+        })
+
+        return self.async_show_form(step_id="user", data_schema=schema, errors=errors)
