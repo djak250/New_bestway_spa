fa6010be07000e5e6854ef9446174b660efee779
M1R4G376 <166696800+M1R4G376@users.noreply.github.com>
2025-07-07 08:37:24 +0200

    Update number.py
    @home409ca #12

--- custom_components/new_bestway_spa/number.py
+++ custom_components/new_bestway_spa/number.py
@@ -7,20 +7,32 @@ async def async_setup_entry(hass, entry, async_add_entities):
     data = hass.data[DOMAIN][entry.entry_id]
     coordinator = data["coordinator"]
     api = data["api"]
+    device_id = entry.title.lower().replace(' ', '_') 
     async_add_entities([
-        BestwaySpaTargetTemperature(coordinator, api, entry.title)
+        BestwaySpaTargetTemperature(coordinator, api, entry.title, device_id)
     ])
 
 class BestwaySpaTargetTemperature(CoordinatorEntity, NumberEntity):
-    def __init__(self, coordinator, api, title):
+    def __init__(self, coordinator, api, title, device_id):
         super().__init__(coordinator)
         self._api = api
         self._attr_name = f"{title} Target Temperature"
-        self._attr_unique_id = f"{title.lower().replace(' ', '_')}_temperature_setting"
+        self._attr_unique_id = f"{device_id}_temperature_setting"
+        self._device_id = device_id
         self._attr_native_min_value = 20.0
         self._attr_native_max_value = 40.0
         self._attr_native_step = 0.5
         self._attr_native_unit_of_measurement = "°C"
+
+    @property
+    def device_info(self):
+        return {
+            "identifiers": {(DOMAIN, self._device_id)},
+            "name": self._attr_name.split(" ")[0],  # lub np. self._device_id
+            "manufacturer": "Bestway",
+            "model": "Spa",
+            "sw_version": "1.0"
+        }
 
     @property
     def native_value(self):
